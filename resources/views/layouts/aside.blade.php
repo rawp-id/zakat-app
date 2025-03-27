893b2c74aa7b634316bf73a985a40cd5c48a4342
Rifky Aryo <wpipit35@gmail.com>
2025-03-28 01:30:51 +0700

    update all

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -3,8 +3,8 @@
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="">
-            <h4 class="font-weight-bold">Zakat Darul Muttaqin</h4>
+        <a class="navbar-brand" href="">
+            <h4 class="me-1 font-weight-bold">Zakat Darul Muttaqin</h4>
         </a>
     </div>
     <hr class="horizontal dark mt-0">
